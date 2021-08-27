6f4040357eac8825efd892019b3e3d0233e51f37
@aug_oliv <aug_oliv@hotmail.com>
2021-08-26 21:46:20 -0300

    inicial commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  images: {
+    deviceSizes: [320, 640, 768, 1024, 1600],
+    domains: ["sumitdey.netlify.app"],
+  },
+};
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public'
+  }
+})
